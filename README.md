6ca2e3b2121a5a2244638dc62069066e6fca5509
sigma-- <poornapragna@gmail.com>
2019-01-19 08:43:06 +0530

    Added resize of window for frame display. Added 5 frame navigation. Changed README as needed.

--- README.md
+++ README.md
@@ -16,13 +16,17 @@ This requires opencv(and dependencies like ffmpeg) and python to be installed in
    
    ```.``` for next frame.
    
+   ```m``` to go back 5 frames.
+   
+   ```/``` to go forward 5 frames.
+   
 3. Hit ```'s'``` on the frame from where you want to clip the video.
 
 4. Hit ```'e'``` on the frame where you want the clip to end.
 
 5. Repeat as necessary.
 
-6. Hit ```'q'``` or ```'p'``` to concatenate and save the video in the path specified in .py file.
+6. Hit ```'q'``` or ```'p'``` to concatenate and save the video in the same path as chosen video file.
 
 
 
--- viditcv.py
+++ viditcv.py
@@ -1,4 +1,3 @@
-import os
 import cv2
 import tkinter as tk
 from tkinter import filedialog
@@ -7,14 +6,15 @@ root = tk.Tk()
 root.withdraw()
 
 file_path = filedialog.askopenfilename()
+out_file = file_path.split('.')[0] + '_out.' + file_path.split('.')[1]
 cap = cv2.VideoCapture(file_path)
-folder = os.path.dirname(file_path)
+
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 print('Frame width = ',frame_width)
 print('Frame height = ',frame_height)
 
-out = cv2.VideoWriter(folder + '/output_video.mp4',cv2.VideoWriter_fourcc(*'mp4v'), 30, (frame_width,frame_height))
+out = cv2.VideoWriter(out_file,cv2.VideoWriter_fourcc(*'mp4v'), 30, (frame_width,frame_height))
 
 pause = False
 start_record = False
@@ -22,17 +22,20 @@ start_frames = []
 end_frames = []
 
 font                   = cv2.FONT_HERSHEY_SIMPLEX
-bottomLeftCornerOfText = (10,500)
+bottomLeftCornerOfText = (10,frame_height - 100)
 fontScale              = 1
 fontColor              = (255,255,255)
 lineType               = 2
 
+cv2.namedWindow('frame',cv2.WINDOW_AUTOSIZE | cv2.WINDOW_KEEPRATIO | cv2.WINDOW_GUI_EXPANDED)
+frame = []
 while(cap.isOpened()):
     if not pause:
         curr_frame_num = cap.get(cv2.CAP_PROP_POS_FRAMES)
         ret, frame = cap.read()
 
-    cv2.imshow('frame',frame)
+    cv2.imshow('frame',cv2.resize(frame, (int(frame_width/2), int(frame_height/2))))
+    
     curr_key = cv2.waitKey(1) & 0xFF
     if curr_key != 255:
         print(curr_key)
@@ -54,10 +57,27 @@ while(cap.isOpened()):
     if curr_key == ord('.'):
         curr_frame_num = cap.get(cv2.CAP_PROP_POS_FRAMES)
         ret, frame = cap.read()
+    if curr_key == ord('m'):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, curr_frame_num - 5)
+        curr_frame_num = cap.get(cv2.CAP_PROP_POS_FRAMES)
+        ret, frame = cap.read()
+    if curr_key == ord('/'):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, curr_frame_num + 5)
+        curr_frame_num = cap.get(cv2.CAP_PROP_POS_FRAMES)
+        ret, frame = cap.read()
     if curr_key == ord('s'):
         start_frames.append(curr_frame_num)
     if curr_key == ord('e'):
         end_frames.append(curr_frame_num)
+
+
+cv2.putText(frame,'Processing...', 
+    bottomLeftCornerOfText, 
+    font, 
+    fontScale*3,
+    fontColor,
+    lineType)
+cv2.imshow('frame',cv2.resize(frame, (int(frame_width/2), int(frame_height/2))))
 
 while(cap.isOpened()):
     for i in range(len(start_frames)):
