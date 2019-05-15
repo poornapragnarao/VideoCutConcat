a4c347317c9d3e5978cba959c8770bc6b326b144
Poornapragna <poornapragna@gmail.com>
2019-05-15 11:11:38 -0400

    Changing to qt GUI

--- viditcv.py
+++ viditcv.py
@@ -1,96 +1,210 @@
+import sys
 import cv2
-import tkinter as tk
-from tkinter import filedialog
+from PyQt5.QtCore import QDir, Qt, QUrl, QThread, pyqtSignal, pyqtSlot
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import (QApplication, QFileDialog, QHBoxLayout, QLabel,
+        QPushButton, QSizePolicy, QSlider, QStyle, QVBoxLayout, QWidget)
 
-root = tk.Tk()
-root.withdraw()
-
-file_path = filedialog.askopenfilename()
-out_file = file_path.split('.')[0] + '_out.' + file_path.split('.')[1]
-cap = cv2.VideoCapture(file_path)
-
-frame_width = int(cap.get(3))
-frame_height = int(cap.get(4))
-print('Frame width = ',frame_width)
-print('Frame height = ',frame_height)
-
-out = cv2.VideoWriter(out_file,cv2.VideoWriter_fourcc(*'mp4v'), 30, (frame_width,frame_height))
-
-pause = False
-start_record = False
+gl_position = 0
+gl_fileName = ''
+gl_state = False
+gl_curr_frame_num = 0 
+cap = []
+ret = 0
+frame = []
 start_frames = []
 end_frames = []
+class Thread(QThread):
+    changePixmap = pyqtSignal(QImage)
 
-font                   = cv2.FONT_HERSHEY_SIMPLEX
-bottomLeftCornerOfText = (10,frame_height - 100)
-fontScale              = 1
-fontColor              = (255,255,255)
-lineType               = 2
+    def run(self):
+        global gl_state, gl_curr_frame_num, cap, ret, frame
+        cap = cv2.VideoCapture(gl_fileName)
+        while True:
+            if gl_state:
+                print('read')
+                gl_curr_frame_num = cap.get(cv2.CAP_PROP_POS_FRAMES)
+                ret, frame = cap.read()
+            if len(frame):
+                rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                h, w, ch = rgbImage.shape
+                bytesPerLine = ch * w
+                convertToQtFormat = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
+                p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+                self.changePixmap.emit(p)
 
-cv2.namedWindow('frame',cv2.WINDOW_AUTOSIZE | cv2.WINDOW_KEEPRATIO | cv2.WINDOW_GUI_EXPANDED)
-frame = []
-while(cap.isOpened()):
-    if not pause:
-        curr_frame_num = cap.get(cv2.CAP_PROP_POS_FRAMES)
-        ret, frame = cap.read()
-
-    cv2.imshow('frame',cv2.resize(frame, (int(frame_width/2), int(frame_height/2))))
+class cvDisp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 Video'
+        self.left = 100
+        self.top = 100
+        self.width = 640
+        self.height = 480
+        
+        self.initUI()
     
-    curr_key = cv2.waitKey(1) & 0xFF
-    if curr_key != 255:
-        print(curr_key)
-    if curr_key == ord('q') or curr_key == ord('p'):
-        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-        break
-    if curr_key == 32:
-        pause = not pause
-        cv2.putText(frame,'| |', 
-            bottomLeftCornerOfText, 
-            font, 
-            fontScale,
-            fontColor,
-            lineType)
-    if curr_key == ord(','):
-        cap.set(cv2.CAP_PROP_POS_FRAMES, curr_frame_num - 1)
-        curr_frame_num = cap.get(cv2.CAP_PROP_POS_FRAMES)
-        ret, frame = cap.read()
-    if curr_key == ord('.'):
-        curr_frame_num = cap.get(cv2.CAP_PROP_POS_FRAMES)
-        ret, frame = cap.read()
-    if curr_key == ord('m'):
-        cap.set(cv2.CAP_PROP_POS_FRAMES, curr_frame_num - 5)
-        curr_frame_num = cap.get(cv2.CAP_PROP_POS_FRAMES)
-        ret, frame = cap.read()
-    if curr_key == ord('/'):
-        cap.set(cv2.CAP_PROP_POS_FRAMES, curr_frame_num + 5)
-        curr_frame_num = cap.get(cv2.CAP_PROP_POS_FRAMES)
-        ret, frame = cap.read()
-    if curr_key == ord('s'):
-        start_frames.append(curr_frame_num)
-    if curr_key == ord('e'):
-        end_frames.append(curr_frame_num)
+    @pyqtSlot(QImage)
+    def setImage(self, image):
+        self.label.setPixmap(QPixmap.fromImage(image))
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.resize(640, 580)
+        # create a label
+        self.label = QLabel(self)
+        self.label.resize(640, 480)
+        fileB = QPushButton("File")
+        fileB.clicked.connect(self.openFile)
+
+        prev5Button = QPushButton("Prev5")
+        prev5Button.clicked.connect(self.previous5Frame)
+
+        prevButton = QPushButton("Prev")
+        prevButton.clicked.connect(self.previousFrame)
+
+        nextButton = QPushButton("Next")
+        nextButton.clicked.connect(self.nextFrame)
+
+        next5Button = QPushButton("Next5")
+        next5Button.clicked.connect(self.next5Frame)
+
+        startB = QPushButton("Start")
+        startB.clicked.connect(self.startBProcess)
+
+        endB = QPushButton("End")
+        endB.clicked.connect(self.endBProcess)
+
+        self.startLabel = QLabel()
+        self.startLabel.resize(400, 20)
+        self.endLabel = QLabel()
+        self.endLabel.resize(400, 20)
+        
+        self.playButton = QPushButton()
+        self.playButton.setEnabled(False)
+        self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+        self.playButton.clicked.connect(self.play)
+
+        self.positionSlider = QSlider(Qt.Horizontal)
+        self.positionSlider.setRange(0, 0)
+        self.positionSlider.sliderMoved.connect(self.setPosition)
+
+        controlLayout = QHBoxLayout()
+        controlLayout.setContentsMargins(0, 0, 0, 0)
+        controlLayout.addWidget(prev5Button)        
+        controlLayout.addWidget(prevButton)
+        controlLayout.addWidget(self.playButton)
+        controlLayout.addWidget(nextButton)
+        controlLayout.addWidget(next5Button)   
+        startLayout = QHBoxLayout()
+        startLayout.addWidget(startB)
+        startLayout.addWidget(self.startLabel)
+        endLayout = QHBoxLayout()
+        endLayout.addWidget(endB)
+        endLayout.addWidget(self.endLabel)     
+
+        layout = QVBoxLayout()
+        layout.addWidget(fileB)
+        layout.addWidget(self.label)
+        layout.addLayout(controlLayout)
+        layout.addLayout(startLayout)
+        layout.addLayout(endLayout)
+        layout.addWidget(self.positionSlider)
 
 
-cv2.putText(frame,'Processing...', 
-    bottomLeftCornerOfText, 
-    font, 
-    fontScale*3,
-    fontColor,
-    lineType)
-cv2.imshow('frame',cv2.resize(frame, (int(frame_width/2), int(frame_height/2))))
+        self.setLayout(layout)
 
-while(cap.isOpened()):
-    for i in range(len(start_frames)):
-        start_frame = start_frames[i]
-        cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
-        end_frame = end_frames[i]
-        curr_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
-        while curr_frame < end_frame:
-            curr_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
-            ret, frame = cap.read()
-            out.write(frame)
-    break
 
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+    
+    def openFile(self):
+        global gl_fileName, gl_state
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open file")
+        gl_fileName = fileName
+
+        if fileName != '':
+            self.playButton.setEnabled(True)
+            th = Thread(self)
+            th.changePixmap.connect(self.setImage)
+            th.start()
+    
+    def previousFrame(self):
+        global cap,ret,frame, gl_curr_frame_num, gl_state
+        gl_state = False
+        cap.set(cv2.CAP_PROP_POS_FRAMES, gl_curr_frame_num - 1)
+        gl_curr_frame_num = cap.get(cv2.CAP_PROP_POS_FRAMES)
+        ret, frame = cap.read()
+
+    def nextFrame(self):
+        global cap,ret,frame, gl_curr_frame_num, gl_state
+        gl_state = False
+        cap.set(cv2.CAP_PROP_POS_FRAMES, gl_curr_frame_num + 1)
+        gl_curr_frame_num = cap.get(cv2.CAP_PROP_POS_FRAMES)
+        ret, frame = cap.read()
+
+
+    def previous5Frame(self):
+        global cap,ret,frame, gl_curr_frame_num, gl_state
+        gl_state = False
+        cap.set(cv2.CAP_PROP_POS_FRAMES, gl_curr_frame_num - 5)
+        gl_curr_frame_num = cap.get(cv2.CAP_PROP_POS_FRAMES)
+        ret, frame = cap.read()
+
+
+    def next5Frame(self):
+        global cap,ret,frame, gl_curr_frame_num, gl_state
+        gl_state = False        
+        cap.set(cv2.CAP_PROP_POS_FRAMES, gl_curr_frame_num + 5)
+        gl_curr_frame_num = cap.get(cv2.CAP_PROP_POS_FRAMES)
+        ret, frame = cap.read()
+
+
+    def startBProcess(self):
+        global start_frames, gl_curr_frame_num
+        start_frames.append(gl_curr_frame_num)
+        self.startLabel.setText(str(start_frames))
+    
+    def endBProcess(self):
+        global end_frames, gl_curr_frame_num
+        end_frames.append(gl_curr_frame_num)
+        self.endLabel.setText(str(end_frames))
+
+    def play(self):
+        global gl_state
+        if gl_state:
+            gl_state = False
+            self.playButton.setIcon(
+                    self.style().standardIcon(QStyle.SP_MediaPlay))
+        else:
+            gl_state = True
+            self.playButton.setIcon(
+                    self.style().standardIcon(QStyle.SP_MediaPause))
+
+    def positionChanged(self, position):
+        global gl_position
+        gl_position = position
+        print(position)
+        print('pc')
+        self.positionSlider.setValue(position)
+
+    def durationChanged(self, duration):
+        self.positionSlider.setRange(0, duration)
+
+    def setPosition(self, position):
+        global gl_position
+        gl_position = position
+        print(position)
+        print('sp')
+        self.mediaPlayer.setPosition(position)
+
+    def handleError(self):
+        self.playButton.setEnabled(False)
+        self.errorLabel.setText("Error: " + self.mediaPlayer.errorString())
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    guiFunc = cvDisp()
+    guiFunc.show()
+    sys.exit(app.exec_())
